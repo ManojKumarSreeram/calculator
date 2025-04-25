4e76a341936c48d2b476841c2080bc5caa2102f5
Manoj <133806364+ManojKumarSreeram@users.noreply.github.com>
2025-04-25 15:25:41 +0530

    Update main.py file where imported mul function from mul.py file

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
 from add import sum
 from sub import min
+from mul import mul
 
 a=int(input("Enter a value"))
 b=int(input("Enter b value"))
 
 sum_value=sum(a,b)
 min_value=min(a,b)
-print(sum_value,"------",min_value)
+mul_value=mul(a,b)
+print(sum_value,"------",min_value,"------",mul_value)
