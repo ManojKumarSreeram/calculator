19fcbb5ce63a2c80ed1bbf35dc0f40ee35e61dc5
ManojKumarSreeram <sreerammanoj777@gmail.com>
2025-04-25 15:07:56 +0530

    added the addition and subtraction files

---
+++ main.py
@@ -0,0 +1,9 @@
+from add import sum
+from sub import min
+
+a=int(input("Enter a value"))
+b=int(input("Enter b value"))
+
+sum_value=sum(a,b)
+min_value=min(a,b)
+print(sum_value,"------",min_value)
